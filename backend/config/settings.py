e57c0911c0f32e36343b5e03d6708ed6d5a46b35
Muhammad Ali <xaydarovmuhammadali350@gmail.com>
2026-08-13 23:08:32 +0500

    STATIC_ROOT sozlandi

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -83,6 +84,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Rasm yuklash: original loyihadagi kabi /uploads/ prefiksi bilan ochiladi
 MEDIA_URL = "/uploads/"
