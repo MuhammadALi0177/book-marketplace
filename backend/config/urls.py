e846b4c688ddbf202453ab62ef3b4efa9d7b5110
Muhammad Ali <xaydarovmuhammadali350@gmail.com>
2026-08-15 15:37:48 +0500

    fix media uploads on render

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls")),
+    re_path(
+        r"^uploads/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
 ]
 
 # Rasm fayllar /uploads/... orqali xizmat qiladi (dev uchun; productionda Nginx/S3 tavsiya etiladi)
