da9769035315945efaa7b0f480634fec8e255416
Muhammad Ali <xaydarovmuhammadali350@gmail.com>
2026-08-16 16:33:01 +0500

    bigger logo

--- frontend/src/components/CatalogPage.vue
+++ frontend/src/components/CatalogPage.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="page">
     <div class="brand-bar">
-      <img class="brand-logo" src="/logo.png" alt="KitobJavon" @error="($e)=>$e.target.style.display='none'" />
-      <div class="brand-text">Kitob<span>Javon</span></div>
+      <img class="brand-logo" src="/logo.png" alt="KitobJavon" />
     </div>
     <div class="page-head">
       <div>
